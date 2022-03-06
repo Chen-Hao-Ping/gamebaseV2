12aa019922c543d2cadbf0dd1782b8c837ebd7be
aria900502 <aria900502@gmail.com>
2022-03-06 23:22:05 +0800

    細節修改

--- i2cFunctionV2.py
+++ i2cFunctionV2.py
@@ -1,19 +1,13 @@
-import re
-import smbus
-import time
 import i2c_IO as i2c
-import i2c_class as deviceMsg
-
-bus = smbus.SMBus(1) #set Master
+import i2c_class as DeviceMsg
 
 slaveAddressMsg = []
-def findAddress(): #return address arrar
-    #slaveAddress = [] #slaveAddress ->devicMsg 的物件陣列
+def findAddress(): #return address arrary
     for address in range(1,11):
         try:
-            bus.write_byte(int(address),int("0")) #傳入0作為連線判斷及啟動連線
+            i2c.send(address,0)
             order = i2c.read(address) #arduino 回傳他目前位置
-            slaveAddressMsg.append(deviceMsg(order,address))
+            slaveAddressMsg.append(DeviceMsg.deviceMsg(order,address))
         except:
             print("somethong was wrong on address:" +str(address))
     slaveAddressMsg.sort(key=lambda x: x.order)
--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import gttsFunction as gF
 if __name__ == '__main__':
 	print("Hello word")
 	gF.speak("apple",'en')
+	#sendWord("apple")
+    i = 3
+    while 1:
+        i2c.send(i,'a')
+        i2c.read(i)
+        i = (i+1)%3
+        #time.sleep(1)
