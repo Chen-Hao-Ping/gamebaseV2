0f7bd804285c4da94a653fa58ff98d2d4a49c3ce
aria900502 <aria900502@gmail.com>
2022-03-09 17:46:59 +0800

    Update i2c_IO_V3.py

--- i2c_IO_V3.py
+++ i2c_IO_V3.py
@@ -1,16 +1,6 @@
 import smbus
 
 bus = smbus.SMBus(1) #set Master
-'''
-value ->0 =>請求order     ascii 30
-        1 =>請求Frist 裝置 ascii 31
-        2 =>請求Secend裝置 ascii 32
-        65-90 =>大寫英文  
-        97-112=>小寫英文 -97  0-26 =>a-z  30-32 =>指令
-        ord('a') => 97 
-        char(97) => a
-        i2c 位元 ->0 -255
-'''
 def send(address,firstOrSec,value): #傳入地址與資訊 ＃0 or 字母
     firstOrSec = firstOrSec
     try:
