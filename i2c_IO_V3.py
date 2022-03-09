9dc475592e88f0d288b670c5350f2e3fa21e8667
aria900502 <aria900502@gmail.com>
2022-03-09 18:53:14 +0800

    Update i2c_IO_V3.py

--- i2c_IO_V3.py
+++ i2c_IO_V3.py
@@ -1,5 +1,15 @@
 import smbus
-
+from interval import Interval
+'''
+value ->0 =>請求order     ascii 30
+        1 =>請求Frist 裝置 ascii 31
+        2 =>請求Secend裝置 ascii 32
+        65-90 =>大寫英文  
+        97-112=>小寫英文 -97  0-26 =>a-z  30-32 =>指令
+        ord('a') => 97 
+        char(97) => a
+        i2c 位元 ->0 -255
+'''
 bus = smbus.SMBus(1) #set Master
 def send(address,firstOrSec,value): #傳入地址與資訊 ＃0 or 字母
     firstOrSec = firstOrSec
@@ -8,7 +18,10 @@ def send(address,firstOrSec,value): #傳入地址與資訊 ＃0 or 字母
             bus.write_byte(int(address), int(str(1)+str(ord(value))))
         elif firstOrSec == 2:
             bus.write_byte(int(address), int(str(2)+str(ord(value))))
-        
+        elif (type(firstOrSec) == int) & Interval(0,26):
+            bus.write_byte(int(address), ord(value))
+        else:
+            print("firstOrSec type is error")
         print ('raspi2arduino: ', value)
     except:
         print ("Write_except")
