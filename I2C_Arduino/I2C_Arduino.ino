86adc9eb6a455e7b3d373b347f216ed338991413
aria900502 <aria900502@gmail.com>
2022-03-10 11:46:24 +0800

    Update I2C_Arduino.ino

--- I2C_Arduino/I2C_Arduino.ino
+++ I2C_Arduino/I2C_Arduino.ino
@@ -1,10 +1,9 @@
 #include <Wire.h>
 
 int SLAVE_ADDRESS = 1;  // 設定Arduino開發板I2C的位址
-int number = 0;
-int state = 0;
-int ii = 0;
-
+int number = 255;
+char firstWord = 0;
+char secWord = 0;
 void setup() {
   //i2cFuction i2cF();
   Serial.begin(9600);   // Serial通訊埠通訊設為9600速率
@@ -17,23 +16,38 @@ void setup() {
 
 void loop() {
 }
+
+
+void getFirst(){
+  
+  }
+void getSec(){
+  
+  }
 void receiveData(int byteCount){
 while(Wire.available()) {
     //當I2C的buffer中有資料時進入迴圈
   number = Wire.read();   //指定nubmer 等於讀取的訊息
+  if(number>=100 && number<200){
+    getFirst();
+    }else if(number>=200){
+      getSec();
+      }else if(number == 255){
+        Serial.println("something is wrong!");
+        }else if(number == 30){
+          //請求order
+          }else if(number == 31){
+            //請求First卡槽
+            }else if(number == 32){
+            //請求Sec卡槽
+            }else if(number == 33){
+            //全部動作
+            }
   Serial.print("data received: ");
   Serial.println(number);
   }
 }
 void sendData(){
-  if(number == 97){
-    Wire.write(SLAVE_ADDRESS+1);
-    ii = 1;
-    SLAVE_ADDRESS = (SLAVE_ADDRESS+1)%3;
-    delay(100);
-    
-   }else
-    ii = 0;
-  
-  Wire.begin(SLAVE_ADDRESS); 
+    Wire.write(SLAVE_ADDRESS);
+    Wire.begin(SLAVE_ADDRESS); 
 }
